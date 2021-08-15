43f929e9c8f7df5c9a2d096d419726230f767406
Lincoln Stoll <me@lds.li>
2021-08-15 12:43:10 +0200

    Finish of skeleton for app attest code

---
+++ appattest/attest.go
@@ -0,0 +1,16 @@
+package appattest
+
+/*
+#cgo CFLAGS: -x objective-c -mmacosx-version-min=10.13 -fobjc-arc
+#cgo LDFLAGS: -framework Foundation -framework DeviceCheck
+#import <Foundation/Foundation.h>
+#import <DeviceCheck/DCAppAttestService.h>
+#import <stdlib.h>
+
+#import "attest.h"
+*/
+import "C"
+
+func TestOne() {
+
+}
---
+++ examples/attested-app/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/lstoll/gosep/appattest"
+
+func main() {
+	appattest.TestOne()
+}
